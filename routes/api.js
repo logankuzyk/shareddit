ba1c864a0ad0a7ba62460379e6657e6150e7a275
Logan Kuzyk <logan@kuzyk.net>
2020-06-30 19:19:51 -0700

    Clarification

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ router.get(
     let data = await backend.generate(req.params);
     res.render("layout", {
       title: data.title,
+      // Render the page, right now it just displays the Imgur image. Probably want to pass the frontend off to a different route.
       body: "<img src=" + '"' + data.url + '">',
     });
   }
