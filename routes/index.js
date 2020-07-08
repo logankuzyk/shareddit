675c8ad2d1e599c50d0c05bd6c1fdae62db95bd0
Logan Kuzyk <logan@kuzyk.net>
2020-07-08 16:05:29 -0700

    New title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("site/index", {
+    title: "shareddit: share reddit posts with images",
+  });
 });
 
 module.exports = router;
