f836dccd1fd38730ea855fbea844bb289e15ff02
Logan Kuzyk <logan@kuzyk.net>
2020-07-08 17:46:31 -0700

    Frontend with basic functionality

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const backend = require("../api/main");
+const handlebars = require("handlebars");
 
 router.get(
   "/:sub/comments/:postID/:title/:commentID?",
   async (req, res, next) => {
     let data = await backend.generate(req.params);
-    res.render("layout", {
+    res.render("site/generated", {
       title: data.title,
       // Render the page, right now it just displays the Imgur image. Probably want to pass the frontend off to a different route.
-      body: "<img src=" + '"' + data.url + '">',
+      link: "<img src='" + data.url + "'/>",
     });
   }
 );
