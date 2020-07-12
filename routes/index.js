15f225aab6bf63b3341be8f531fbf308c3a17b06
Logan Kuzyk <logan@kuzyk.net>
2020-07-11 21:37:06 -0700

    Add a header and some basic info

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("site/index", {
     title: "shareddit: share reddit posts with images",
-    body: "",
+    info: fs.readFileSync(__dirname + "/../views/site/info.hbs"),
   });
 });
 
