af88cb2e4c3c340c424116754909da27ced62628
Logan Kuzyk <logan@kuzyk.net>
2021-05-29 14:48:44 -0700

    Remove dist

--- dist/shared/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// Put shared constants here
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.paramMissingError = void 0;
-exports.paramMissingError = 'One or more of the required parameters was missing.';
