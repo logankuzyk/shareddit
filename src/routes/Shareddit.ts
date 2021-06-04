8a45ba493fdee9885350d7fa8a3380f24001aac1
Logan Kuzyk <logan@kuzyk.net>
2021-06-03 17:14:17 -0700

    Remove JS doc comments

--- src/routes/Shareddit.ts
+++ src/routes/Shareddit.ts
@@ -1,34 +1,35 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response } from 'express';
 
-import renderImage from '../business/renderImage'
-import { FleshedRedditSubmission, SkeletonRedditSubmission } from 'src/business/types';
+import renderImage from '../business/renderImage';
+import {
+  FleshedRedditSubmission,
+  SkeletonRedditSubmission,
+} from 'src/business/types';
 import { ParamsDictionary } from 'express-serve-static-core';
 
 const { BAD_REQUEST, CREATED, OK } = StatusCodes;
 
-const validateParams = async (params: ParamsDictionary): Promise<SkeletonRedditSubmission> => {
-    const output: SkeletonRedditSubmission = {
-        sub: params.sub,
-        postID: params.postID,
-        urlTitle: params.title ? params.title : undefined,
-        commentID: params.commentID ? params.commentID : undefined,
-        redact: params.redact ? true : false,
-    }
+const validateParams = async (
+  params: ParamsDictionary
+): Promise<SkeletonRedditSubmission> => {
+  const output: SkeletonRedditSubmission = {
+    sub: params.sub,
+    postID: params.postID,
+    urlTitle: params.title ? params.title : undefined,
+    commentID: params.commentID ? params.commentID : undefined,
+    redact: params.redact ? true : false,
+  };
 
-    return output;
-}
+  return output;
+};
 
-/**
- * Determine the given parameters, call the backend and return the URL for an image.
- * 
- * @param req 
- * @param res 
- */
 export const getSharedditImage = async (req: Request, res: Response) => {
-    const { params } = req;
-    const generationParams: SkeletonRedditSubmission = await validateParams(params);
-    const imageURL = await renderImage(generationParams);
+  const { params } = req;
+  const generationParams: SkeletonRedditSubmission = await validateParams(
+    params
+  );
+  const imageURL = await renderImage(generationParams);
 
-    return res.status(OK).json({ image: imageURL });
-}
+  return res.status(OK).json({ image: imageURL });
+};
