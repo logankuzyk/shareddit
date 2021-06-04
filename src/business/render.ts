53c1c7c0051b36d34e25a836a06484e74dd0b695
Logan Kuzyk <logan@kuzyk.net>
2021-06-03 19:50:32 -0700

    Get rid of selfText template

--- src/business/render.ts
+++ src/business/render.ts
@@ -1,11 +1,27 @@
 import handlebars from 'handlebars';
 import fs from 'fs';
+
 import { RenderTypes } from './types';
 
-const Render = (templateName: RenderTypes): HandlebarsTemplateDelegate<any> => {
-  return handlebars.compile(
-    fs.readFileSync(__dirname + `/templates/${templateName}`)
-  );
+const Render: RenderTypes = {
+  award: handlebars.compile(
+    fs.readFileSync(__dirname + '/templates/award.hbs', 'utf8')
+  ),
+  comment: handlebars.compile(
+    fs.readFileSync(__dirname + '/templates/comment.hbs', 'utf8')
+  ),
+  everything: handlebars.compile(
+    fs.readFileSync(__dirname + '/templates/everything.hbs', 'utf8')
+  ),
+  imageSubmission: handlebars.compile(
+    fs.readFileSync(__dirname + '/templates/imageSubmission.hbs', 'utf8')
+  ),
+  textSubmission: handlebars.compile(
+    fs.readFileSync(__dirname + '/templates/textSubmission.hbs', 'utf8')
+  ),
+  // selfText: handlebars.compile(
+  //   fs.readFileSync(__dirname + '/templates/selfText.hbs', 'utf8')
+  // ),
 };
 
 export default Render;
