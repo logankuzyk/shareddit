c14b9cd43e2255a0ca9cb92370779621639de1a7
Logan Kuzyk <logan@kuzyk.net>
2021-06-05 14:20:42 -0700

    Use query-string, set up redirect for old shareddit image endpoint

--- src/routes/Shareddit.ts
+++ src/routes/Shareddit.ts
@@ -1,5 +1,6 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response } from 'express';
+import queryString from 'query-string';
 
 import renderImage from '../business/renderImage';
 import {
@@ -11,7 +12,7 @@ import { ParamsDictionary } from 'express-serve-static-core';
 const { BAD_REQUEST, CREATED, OK } = StatusCodes;
 
 const validateParams = async (
-  params: ParamsDictionary
+  params: ParamsDictionary | queryString.ParsedQuery<any>
 ): Promise<SkeletonRedditSubmission> => {
   const output: SkeletonRedditSubmission = {
     sub: params.sub,
@@ -24,15 +25,32 @@ const validateParams = async (
   return output;
 };
 
-export const getSharedditImage = async (req: Request, res: Response) => {
-  const { params } = req;
-  const generationParams: SkeletonRedditSubmission = await validateParams(
-    params
-  );
-
+export const parseQueryString = async (req: Request, res: Response) => {
   try {
+    const query = req.path;
+    const params = queryString.parse(query);
+    const generationParams: SkeletonRedditSubmission = await validateParams(
+      params
+    );
+
     const imageElement = await renderImage(generationParams);
     return res.send(imageElement);
+  } catch (err) {
+    console.error(err);
+    return res.status(500);
+  }
+};
+
+export const getSharedditImage = async (req: Request, res: Response) => {
+  try {
+    const { params } = req;
+    const generationParams: SkeletonRedditSubmission = await validateParams(
+      params
+    );
+
+    const query = queryString.stringify(params, { skipNull: true });
+
+    return res.redirect(`/${query}`);
   } catch (err) {
     console.error(err);
     return res.status(500);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
-import { getSharedditImage } from './Shareddit';
+import { getSharedditImage, parseQueryString } from './Shareddit';
 
 const sharedditRouter = Router();
-sharedditRouter.get('/r/:sub/comments/:postID', getSharedditImage);
+
+sharedditRouter.get('/r/:sub/comments/:postID/(:title)?', getSharedditImage);
 sharedditRouter.get(
   '/r/:sub/comments/:postID/:title/:commentID',
   getSharedditImage
 );
+
+sharedditRouter.get('/*', parseQueryString);
 
 // Export the base-router
 const baseRouter = Router();
