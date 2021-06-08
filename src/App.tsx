efa14aeaf48aa89e38fd9c19ea3824e4553a5a59
Logan Kuzyk <logan@kuzyk.net>
2021-06-08 10:17:56 -0700

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
@@ -8,9 +8,9 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./components/ColorModeSwitcher"
-import { Logo } from "./components/Logo"
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./components/ColorModeSwitcher";
+import { Logo } from "./components/Logo";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -35,4 +35,4 @@ export const App = () => (
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
