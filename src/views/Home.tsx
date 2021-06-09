dac1196ce77c997755b25219f8941f65b7070f24
Logan Kuzyk <logan@kuzyk.net>
2021-06-09 10:43:20 -0700

    Add title for homepage

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box, Text, VStack, Grid, Input, Button } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Box, Text, VStack, Grid, Button } from "@chakra-ui/react";
 
 import { GenerateButton } from "../components/GenerateButton";
 import { RedditLinkInput } from "../components/RedditLinkInput";
@@ -9,6 +9,10 @@ import { ArrowDownIcon } from "@chakra-ui/icons";
 interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = ({}) => {
+  useEffect(() => {
+    document.title = "shareddit";
+  }, []);
+
   return (
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
