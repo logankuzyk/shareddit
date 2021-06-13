f9c02ad56000c01f0f89802e1e2d9d36822c918a
Logan Kuzyk <logan@kuzyk.net>
2021-06-12 17:15:24 -0700

    Redirect to query URL on submit

--- src/submitLink.ts
+++ src/submitLink.ts
@@ -2,10 +2,14 @@ import queryString from "query-string";
 import { UrlParser } from "url-params-parser";
 
 export const submitLink = (input: string) => {
-  const params = UrlParser(
-    input,
-    "/r/:sub/comments/:postID/:title/:commentID"
-  ).namedParams;
-  const query = queryString.stringify(params);
-  //   window.history.replaceState(null, document.title, query);
+  try {
+    const params = UrlParser(
+      input,
+      "/r/:sub/comments/:postID/:title/:commentID"
+    ).namedParams;
+    const query = queryString.stringify(params);
+    window.location.pathname = query;
+  } catch (err) {
+    console.error(err);
+  }
 };
