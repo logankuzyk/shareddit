89d1ad2511977eb96b277305121d8de09bbf6d03
Logan Kuzyk <logan@kuzyk.net>
2021-06-15 13:50:04 -0700

    Set up CORS running on port 3001 and getting requests from 3000

--- src/Server.ts
+++ src/Server.ts
@@ -1,14 +1,14 @@
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
-import path from "path";
-import helmet from "helmet";
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import path from 'path';
+import helmet from 'helmet';
 
-import express, { NextFunction, Request, Response } from "express";
-import StatusCodes from "http-status-codes";
-import "express-async-errors";
+import express, { NextFunction, Request, Response } from 'express';
+import StatusCodes from 'http-status-codes';
+import 'express-async-errors';
 
-import BaseRouter from "./routes";
-import logger from "@shared/Logger";
+import BaseRouter from './routes';
+import logger from '@shared/Logger';
 
 const app = express();
 const { BAD_REQUEST } = StatusCodes;
@@ -20,19 +20,27 @@ const { BAD_REQUEST } = StatusCodes;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 // Show routes called in console during development
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Security
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   app.use(helmet());
 }
 
 // Add APIs
-app.use("/", BaseRouter);
+app.use('/', BaseRouter);
 
 // Print API errors
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
