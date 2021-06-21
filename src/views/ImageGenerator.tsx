0a1d45a22363dee1a5f82eb40dccb66e17c17d90
Logan Kuzyk <logan@kuzyk.net>
2021-06-21 10:57:16 -0700

    Fix ImageGenerator view

--- src/views/ImageGenerator.tsx
+++ src/views/ImageGenerator.tsx
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
-import { Box, Grid, VStack, Text } from "@chakra-ui/react";
+import React from "react";
+import { Box, Grid, VStack } from "@chakra-ui/react";
 import Helmet from "react-helmet";
 
 import { SharedditLogo } from "../components/SharedditLogo";
-import { getParams } from "../util/getParams";
 import { Editor } from "../components/Editor";
-import { useEffect } from "react";
 
 export const ImageGenerator: React.FC = () => {
-  const [params, setParams] = useState({});
-
-  useEffect(() => {
-    getParams().then((urlParams) => {
-      if (urlParams !== null) setParams(urlParams);
-      else
-        setParams({
-          message:
-            "Something went wrong, make sure you copied the reddit URL correctly.",
-        });
-    });
-  }, []);
-
   return (
     <Box textAlign="center" fontSize="xl">
       <Helmet>
