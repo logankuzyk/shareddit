b5f656827839ac77c1d63553fe2d4659ada8e7cf
Logan Kuzyk <logan@kuzyk.net>
2021-06-22 11:33:25 -0700

    Change Template CSS based on what reddit theme is being used

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -4,6 +4,21 @@ import React, { useState } from "react";
 import { Comment } from "./templates/Comment";
 import { FleshedRedditSubmission, ImageTheme } from "../types";
 import { ImageSubmission } from "./templates/ImageSubmission";
+
+type Styles = {
+  [key in ImageTheme]: object;
+}
+
+const styles: Styles = {
+  old: {
+    backgroundColor: "white",
+    textAlign: "left",
+    fontFamily: "verdana, arial, helvetica, sans-serif",
+    fontSize: "x-small",
+    lineHeight: "normal",
+  },
+  new: {},
+}
 interface TemplateProps {
   content: FleshedRedditSubmission;
 }
@@ -11,16 +26,10 @@ interface TemplateProps {
 export const Template: React.FC<TemplateProps> = ({ content }) => {
   const [theme, setTheme] = useState<ImageTheme>("old");
   const [type, setType] = useState(content.type);
-  console.log(content);
+
   return (
     <Box
-      style={{
-        backgroundColor: "white",
-        textAlign: "left",
-        fontFamily: "verdana, arial, helvetica, sans-serif",
-        fontSize: "x-small",
-        lineHeight: "normal",
-      }}
+      style={styles[theme]}
     >
       <ImageSubmission post={content} />
       {content.comments.length > 0 ? (
