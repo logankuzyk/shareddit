b2feed93feca037e69a868545fff98d703f9738c
Logan Kuzyk <logan@kuzyk.net>
2021-06-22 15:27:58 -0700

    Remove fluff

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,12 +3,8 @@ import { Box, Grid, VStack, Text } from "@chakra-ui/react";
 
 import RedditContextProvider from "./RedditContext";
 import { Template } from "./Template";
-import { FleshedRedditSubmission } from "../types";
 
 export const Editor: React.FC = () => {
-  const [params, setParams] = useState<FleshedRedditSubmission | null>(null);
-  const [error, setError] = useState<{ message: string }>({ message: "" });
-
   return (
     <Box textAlign="center" fontSize="xl" width="100%">
       <Grid p={3}>
