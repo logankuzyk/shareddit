47f5584090be33759ac3cba6a76ba5d3900dc32b
Logan Kuzyk <logan@kuzyk.net>
2021-06-23 11:08:02 -0700

    Add redact colors for comments and submissions

--- src/types.ts
+++ src/types.ts
@@ -18,6 +18,7 @@ export interface RedditComment {
   bodyHTML: string;
   awards: Award[];
   parentID: string;
+  color: string | null;
 }
 export interface FleshedRedditSubmission {
   author: string;
@@ -32,6 +33,7 @@ export interface FleshedRedditSubmission {
   type: "image" | "link" | "text" | "album";
   redact: boolean;
   sub: string;
+  color: string | null;
 }
 
 export interface BackendResponse extends FleshedRedditSubmission {
