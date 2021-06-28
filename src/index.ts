98fcdc34a7bfa5b91bb2e010d447a8f72af1c362
Logan Kuzyk <logan@kuzyk.net>
2021-06-28 14:01:53 -0700

    Port 80 so I can use HTTPS

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import logger from '@shared/Logger';
 const dotenv = require('dotenv').config();
 
 // Start the server
-const port = Number(8080);
+const port = Number(80);
 app.listen(port, () => {
   logger.info('Express server started on port: ' + port);
 });
