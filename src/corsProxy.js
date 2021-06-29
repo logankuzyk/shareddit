9fcfd6d56f334c5c897dcc914341399d97a3daa2
Logan Kuzyk <logan@kuzyk.net>
2021-06-29 11:05:19 -0700

    Update CORS anywhere settings to match Heroku demo

--- src/corsProxy.js
+++ src/corsProxy.js
@@ -4,7 +4,20 @@ module.exports.start = () => {
   corsProxy
     .createServer({
       originWhitelist: [],
-      requireHeader: [],
+      requireHeader: ['origin', 'x-requested-with'],
+      removeHeaders: [
+        'cookie1',
+        'cookie2',
+        'x-request-start',
+        'x-request-id',
+        'via',
+        'connect-time',
+        'total-route-time',
+      ],
+      redirectSameOrigin: true,
+      httpProxyOptions: {
+        xfwd: true,
+      },
     })
     .listen(8080, '0.0.0.0', console.log('CORS Anywhere started'));
 };
