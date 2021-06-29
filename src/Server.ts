e38e84dad847ea3df80d4444f164773150bcc509
Logan Kuzyk <logan@kuzyk.net>
2021-06-29 00:29:02 +0000

    FINALLY FIX HTTPS

--- src/Server.ts
+++ src/Server.ts
@@ -18,11 +18,15 @@ const dotenv = require('dotenv').config();
  *                              Set basic express settings
  ***********************************************************************************/
 
+app.enable('trust proxy');
+app.use((req, res, next) => {
+	req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND);
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
