2497a507339a198bfd46df09f01779476f5a6fd9
Logan Kuzyk <logan@kuzyk.net>
2021-06-30 12:40:20 -0700

    Add awards badge

---
+++ src/components/templates/Awards.tsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { Box } from "@chakra-ui/react";
+
+import { Award } from "../../types";
+import { RedditContext } from "../RedditContext";
+import { colors } from "./styles";
+
+interface AwardsProps {
+  awards: Award[];
+}
+export const Awards: React.FC<AwardsProps> = ({ awards }) => {
+  const { darkMode } = useContext(RedditContext);
+  let count = 0;
+
+  return (
+    <Box
+      style={{
+        display: "flex",
+        borderRadius: "12px",
+        borderWidth: 1,
+        color: colors(darkMode).color,
+        borderColor: colors(darkMode).borderColor,
+        backgroundColor: colors(darkMode).iconBackgroundColor,
+        paddingTop: 2,
+        paddingBottom: 2,
+        paddingLeft: 4,
+        paddingRight: 4,
+        fontSize: 12,
+      }}
+    >
+      {awards.map((award) => {
+        count += award.count;
+        return (
+          <img
+            height={16}
+            width={16}
+            src={`https://server.shareddit.com:8080/${award.src}`}
+          />
+        );
+      })}
+      {` ${count} ${count === 1 ? "award" : "awards"}`}
+    </Box>
+  );
+};
