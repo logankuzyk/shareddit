12de2dcb3d5b48e9be8d79b4a957ef2f00ff5ec5
Logan Kuzyk <logan@kuzyk.net>
2021-07-05 13:12:31 -0700

    Add donate modal boilerplate

---
+++ src/components/DonateModal.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+import { Button } from "@chakra-ui/button";
+import { useDisclosure } from "@chakra-ui/hooks";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+} from "@chakra-ui/modal";
+
+import { Kofi } from "./Kofi";
+
+export const DonateModal: React.FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button
+        as={FaHeart}
+        color="#CC3300"
+        backgroundColor="transparent"
+        onClick={onOpen}
+      ></Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>";asldgha;dfjh45o3iu"</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
