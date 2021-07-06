c2d67726da97f3b3d576a06c1df4319b69eb30fd
Logan Kuzyk <logan@kuzyk.net>
2021-07-06 13:40:57 -0700

    Add S3 image uploading, open image in new tab on success

--- src/components/ImageOptions.tsx
+++ src/components/ImageOptions.tsx
@@ -16,29 +16,48 @@ export const ImageOptions: React.FC<ImageOptionsProps> = () => {
     const node = document.getElementById("reddit-preview");
 
     if (node === null) {
-      alert("Something went wrong, can't download image.");
+      alert(
+        "There is an issue with the editor. Can't generate or download image."
+      );
       return;
     }
 
     if (downloadAs === "png") {
       htmlToImage.toPng(node).then(async (dataURL) => {
-        // localStorage.setItem("shareddit-image", dataURL);
-        // window.location.href = "/download";
-
-        const data = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-
+        const buf = Buffer.from(
+          dataURL.replace(/^data:image\/(png|jpg);base64,/, ""),
+          "base64"
+        );
         const {
           data: { uploadURL },
-        } = await axios.get("http://localhost:3000/getUploadURL");
+        } = await axios.get("https://server.shareddit.com/getUploadURL");
 
-        const res = await axios.put(uploadURL, data, {
-          headers: {
-            "Content-Encoding": "base64",
-            "Content-Type": "image/png",
-          },
-        });
-
-        console.log(res);
+        if (!uploadURL) {
+          alert(
+            "There is an issue with AWS or the shareddit backend server. Please try again later."
+          );
+          return;
+        } else {
+          axios
+            .put(uploadURL, buf, {
+              headers: {
+                "Content-Encoding": "base64",
+                "Content-Type": "image/png",
+              },
+            })
+            .then((res) => {
+              if (res.status !== 200) {
+                console.log(
+                  dataURL.replace(/^data:image\/(png|jpg);base64,/, "")
+                );
+                alert(
+                  "Something went wrong, can't download image. The base 64 code is available in the console (press F12), you can turn that into an image here: https://codebeautify.org/base64-to-image-converter"
+                );
+              } else {
+                window.open(uploadURL.split("?")[0]);
+              }
+            });
+        }
       });
     }
   };
