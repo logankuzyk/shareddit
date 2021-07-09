e9996025092221d9c0e0eff0ff732be34adad02a
Logan Kuzyk <logan@kuzyk.net>
2021-07-09 10:54:57 -0700

    Image albums now wrap once they're small enough, also scale banner text

--- src/components/templates/Image.tsx
+++ src/components/templates/Image.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Box, Icon } from "@chakra-ui/react";
+import { Box, Icon, WrapItem } from "@chakra-ui/react";
 import { LinkIcon } from "@chakra-ui/icons";
 import { Fade } from "@chakra-ui/transition";
 import { FaImage } from "@react-icons/all-files/fa/FaImage";
@@ -24,34 +24,37 @@ export const Image: React.FC<ImageProps> = ({ src, host, icon }) => {
   };
 
   return (
-    <Fade in={true}>
-      <Box
-        style={{
-          borderRadius: 12,
-          borderWidth: 1,
-          borderColor: colors(darkMode).borderColor,
-          overflow: "hidden",
-          position: "relative",
-          width: options.imageScale,
-          height: options.imageScale,
-        }}
-      >
+    <WrapItem
+      style={{
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: colors(darkMode).borderColor,
+        overflow: "hidden",
+        position: "relative",
+        width: options.imageScale,
+        height: options.imageScale,
+      }}
+    >
+      <Fade in={true}>
         <img
           src={`https://server.shareddit.com:8080/${src}`}
           alt="reddit submission"
         />
-        <Box
-          position="absolute"
-          background="rgba(0, 0, 0, 0.5)"
-          color="#AAAAAA"
-          textAlign="center"
-          bottom={0}
-          width="100%"
-          fontSize={12}
-        >
-          {icons[icon]} {host}
-        </Box>
+      </Fade>
+      <Box
+        position="absolute"
+        background="rgba(0, 0, 0, 0.5)"
+        color="#AAAAAA"
+        textAlign="center"
+        bottom={0}
+        width="100%"
+        fontSize={
+          0.15 *
+          Number(options.imageScale.substr(0, options.imageScale.length - 1))
+        }
+      >
+        {icons[icon]} {host}
       </Box>
-    </Fade>
+    </WrapItem>
   );
 };
