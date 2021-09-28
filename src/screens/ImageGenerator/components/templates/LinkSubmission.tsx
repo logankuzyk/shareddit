b63df0856b22d9ff0cf552df2360f3a820697e7c
Logan Kuzyk <logan@kuzyk.net>
2021-09-28 16:53:26 -0700

    Show title even if there's no thumbnail!

--- src/screens/ImageGenerator/components/templates/LinkSubmission.tsx
+++ src/screens/ImageGenerator/components/templates/LinkSubmission.tsx
@@ -8,18 +8,13 @@ import { FleshedRedditSubmission } from "../../../../types";
 export const LinkSubmission: React.FC<FleshedRedditSubmission> = ({
   link,
   thumbnail,
+  title,
   ...props
 }) => {
-  if (!thumbnail) return <></>;
   const src = Array.isArray(thumbnail) ? thumbnail[0] : thumbnail;
-
   const host = link !== null ? new URL(link).hostname : null;
+  const Content = src ? <Image src={src} host={host} icon="link" /> : undefined;
+  const outputTitle = Content ? title : title.concat(` (${host})`);
 
-  console.log(host);
-
-  return (
-    <>
-      <Title {...props} Content={<Image src={src} host={host} icon="link" />} />
-    </>
-  );
+  return <Title {...props} title={outputTitle} Content={Content} />;
 };
