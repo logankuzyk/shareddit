c24894f112e7f3c8accbc7166909de96a86e0c45
Logan Kuzyk <logan@kuzyk.net>
2022-02-18 14:56:06 -0800

    Call getRedditData

--- functions/fetch-reddit-data/fetch-reddit-data.ts
+++ functions/fetch-reddit-data/fetch-reddit-data.ts
@@ -1,12 +1,18 @@
-import { Handler } from '@netlify/functions'
+import { Handler } from "@netlify/functions";
+import getRedditData from "./getRedditData";
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+  const { postID, commentID, redact, sub } = event.queryStringParameters;
+
+  const body = await getRedditData({
+    postID,
+    commentID,
+    redact: !!redact,
+    sub,
+  });
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${name}!`,
-    }),
-  }
-}
+    body: JSON.stringify(body),
+  };
+};
