169a5399c57658fb0c5b7630bdf0bf9ef742c36f
Logan Kuzyk <logan@kuzyk.net>
2022-02-19 12:06:03 -0800

    Remove useMemo

--- src/screens/RedirectRedditPath.tsx
+++ src/screens/RedirectRedditPath.tsx
@@ -1,10 +1,10 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Redirect } from "react-router";
 import { UrlParser } from "url-params-parser";
 import queryString from "query-string";
 
 export const RedirectRedditPath: React.FC = () => {
-  const path = useMemo(() => window.location.href, [window.location.pathname]);
+  const path = window.location.href;
   const params = UrlParser(
     path,
     "/r/:sub/comments/:postID/:title/:commentID"
