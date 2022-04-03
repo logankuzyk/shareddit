824864d291f65de66b02f91c5ad0908a9bee13bd
Logan Kuzyk <logan@kuzyk.net>
2022-04-03 16:28:37 -0700

    Add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ReactGA from "react-ga";
 
@@ -15,24 +16,28 @@ export const App = () => {
   ReactGA.initialize("UA-51247116-3");
   ReactGA.pageview(window.location.pathname + window.location.search);
 
+  const client = new QueryClient();
+
   return (
     <ChakraProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomeScreenProvider />
-          </Route>
-          <Route path="/generate">
-            <EditorScreenProvider />
-          </Route>
-          <Route path="/r">
-            <RedirectRedditPath />
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <HomeScreenProvider />
+            </Route>
+            <Route path="/generate">
+              <EditorScreenProvider />
+            </Route>
+            <Route path="/r">
+              <RedirectRedditPath />
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 };
