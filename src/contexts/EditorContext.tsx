d2592d910863f5f8a3b281b81cf639a1e6a307cb
Logan Kuzyk <logan@kuzyk.net>
2022-05-07 16:30:03 -0700

    Add simple EditorContext

--- src/contexts/EditorContext.tsx
+++ src/contexts/EditorContext.tsx
@@ -1,1 +1,33 @@
-export {};
+import React, { useContext, createContext } from "react";
+
+export interface EditorContext {
+  fontSize: "small" | "medium" | "large";
+  imageScale: string;
+  censorUsernames: boolean;
+  censorSubreddit: boolean;
+  theme: "light" | "dark";
+  showComments: boolean;
+}
+
+const initialState: EditorContext = {
+  fontSize: "medium",
+  imageScale: "100%",
+  censorUsernames: false,
+  censorSubreddit: false,
+  theme: "light",
+  showComments: true,
+};
+
+const EditorContext = createContext<EditorContext>(initialState);
+
+export const useEditorContext = () => {
+  return useContext(EditorContext);
+};
+
+export const EditorContextProvider: React.FC = ({ children }) => {
+  return (
+    <EditorContext.Provider value={initialState}>
+      {children}
+    </EditorContext.Provider>
+  );
+};
