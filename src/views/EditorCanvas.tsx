deab1bf1254f697ae30dad1ce0582ca5fe9ad869
Logan Kuzyk <logan@kuzyk.net>
2022-05-08 18:06:33 -0700

    Use EditorContextProvider

--- src/views/EditorCanvas.tsx
+++ src/views/EditorCanvas.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Flex } from "@chakra-ui/react";
 
 import { RedditSubmission, RedditComment, MoreChildren } from "../types/reddit";
+import { EditorContextProvider } from "../contexts/EditorContext";
 import { Submission } from "../components/templates/submissions/Submission";
 import { RootComment } from "../components/templates/comments/RootComment";
 
@@ -20,9 +21,12 @@ export const EditorCanvas: React.FC<EditorCanvasProps> = ({
       alignItems="left"
       justifyContent="center"
       maxWidth="40vw"
+      textAlign="left"
     >
-      <Submission submission={submission} />
-      <RootComment data={comments[0]} />
+      <EditorContextProvider>
+        <Submission submission={submission} />
+        <RootComment data={comments[0]} />
+      </EditorContextProvider>
     </Flex>
   );
 };
