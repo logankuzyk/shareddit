ec2aff5c838fbad80e8de93f0c9b7f87882de3fc
Logan Kuzyk <logan@kuzyk.net>
2022-05-11 19:04:25 -0700

    Use theme colors

--- src/components/templates/media/SelfText.tsx
+++ src/components/templates/media/SelfText.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
 
+import { useEditorContext } from "../../../contexts/EditorContext";
 import { RedditSubmission } from "../../../types/reddit";
-import { colors } from "../../../styles/colors";
 import { Paragraph } from "../../typography/Paragraph";
 
 interface SelfTextProps {
@@ -10,7 +10,11 @@ interface SelfTextProps {
 }
 
 export const SelfText: React.FC<SelfTextProps> = ({ submission }) => {
+  const { theme } = useEditorContext();
   const { selftext } = submission;
+
+  const backgroundColor = theme.main["100"];
+  const borderColor = theme.main["300"];
 
   if (selftext) {
     return (
@@ -18,8 +22,8 @@ export const SelfText: React.FC<SelfTextProps> = ({ submission }) => {
         padding={2}
         borderWidth={1}
         borderRadius={4}
-        backgroundColor={colors.grey["100"]}
-        borderColor={colors.grey["300"]}
+        backgroundColor={backgroundColor}
+        borderColor={borderColor}
       >
         <Paragraph>{selftext}</Paragraph>
       </Flex>
