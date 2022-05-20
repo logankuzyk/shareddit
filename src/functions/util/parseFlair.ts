6f48d33a700ac88c92c585bf35ac6312446f10ef
Logan Kuzyk <logan@kuzyk.net>
2022-05-19 18:02:27 -0700

    Make sure flair is defined

--- src/functions/util/parseFlair.ts
+++ src/functions/util/parseFlair.ts
@@ -1,11 +1,11 @@
 import { RawFlair, RedditFlair } from "../../types/reddit";
 
 export const parseFlair = (
-  flair: RawFlair,
+  flair: RawFlair | undefined,
   textColor: string,
   backgroundColor: string
 ): RedditFlair | undefined => {
-  return flair.length === 2
+  return flair && flair.length === 2
     ? {
         img: flair[0].u,
         text: flair[1].t,
